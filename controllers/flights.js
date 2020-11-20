8ad78715775dca22ef3f101ca4f6501c3fe00122
Jonahmallard <jonahmallard@outlook.com>
2020-11-20 17:26:47 -0600

    Finish Refactoring

--- controllers/flights.js
+++ controllers/flights.js
@@ -24,5 +24,10 @@ function create(req, res) {
 }
 
 function newFlight(req, res) {
-    res.render('flights/new');
+    const newFlight = new Flight();
+    // Obtain the default date
+    const dt = newFlight.departs;
+    // Format the date for the value attribute of the input
+    const departsDate = dt.toISOString().slice(0, 16);
+    res.render('flights/new', {departsDate});
 }
